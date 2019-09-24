80cfe55c675ac98ed1246b2f15f23b01653cbf67
caitlining <caitlincing@gmail.com>
2019-09-24 23:36:18 +0000

    modifying our original text and js files to use mocha/chai testing, and making testing files for those tests. Also making a .gitignore file to put the node_modules in

--- syncBreeds.js
+++ syncBreeds.js
@@ -6,12 +6,7 @@ const catBreeds = {
 
 // synchronous function to fetch a cat breed
 const breedDetails = function(breed) {
-  // can simply return it
   return catBreeds[breed];
 };
 
-// get the return value right away from the function
-const bombay = breedDetails('Bombay');
-console.log(bombay);
-
-console.log(breedDetails('Balinese'));
+module.exports = breedDetails;
